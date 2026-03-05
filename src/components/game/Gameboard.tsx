1840f70420c3028b21bb9072a9a4df07fc85a904
Dbrowne14 <davidbrowne1992@gmail.com>
2026-03-05 22:45:11 +0000

    gameboard scomponent change

--- src/components/game/Gameboard.tsx
+++ src/components/game/Gameboard.tsx
@@ -1,10 +1,10 @@
-
+import GameInput from "./GameInput"
 
 const Gameboard = () => {
   return (
     <div className='h-full flex justify-center items-center'>
         <div className='h-160 w-260 border border-white'>
-
+            <GameInput/>
         </div>
     </div>
   )
