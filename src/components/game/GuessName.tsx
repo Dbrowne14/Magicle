63c9e80e772486574f921f06aa2fb61b585a4732
Dbrowne14 <davidbrowne1992@gmail.com>
2026-03-06 18:07:52 +0000

    adding dummy data

--- src/components/game/GuessName.tsx
+++ src/components/game/GuessName.tsx
@@ -1,8 +1,9 @@
 import defaultMTGCard from "../../assets/Magic_card_back.png"
 
 const GuessName = () => {
-  return <div className="h-40 border-b border-white inline-flex px-3 items-center">
+  return <div className="h-40 border-b border-white inline-flex px-3 items-center justify-center gap-4">
     <img src={defaultMTGCard} className="h-[90%]"></img>
+    <h2 className="h-fit text-wrap w-25">Default Card Name XXXX</h2>
   </div>;
 };
 
---
+++ src/data/dummyData.ts
@@ -0,0 +1,22 @@
+type data ={
+    cmc: number;
+    recentPrint: string,
+    rarity: "C" | "U" | "R" | "M",
+    cardType: string,
+    pip: string,
+    printYear: number,
+    colorCount: number,
+    price: number
+}
+
+const dummyData = {
+    cmc: 7,
+    recentPrint: "LOTR",
+    rarity: "M",
+    cardType: "sorcery",
+    pip: "Green",
+    printYear: 2023,
+    colorCount: 1,
+    price: 20.00,
+    
+}
