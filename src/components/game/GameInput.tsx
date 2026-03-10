8058e4a8c84f940970b8103e43ce5ab3e01bea3a
Dbrowne14 <davidbrowne1992@gmail.com>
2026-03-10 22:08:14 +0000

    changes to input and types

--- src/components/game/GameInput.tsx
+++ src/components/game/GameInput.tsx
@@ -1,10 +1,16 @@
+type GuessProps = {
+  setGuessState: React.Dispatch<React.SetStateAction<string>>;
+};
 
-const GameInput = ({setGuessState}) => {
+const GameInput = ({ setGuessState }:GuessProps) => {
   return (
     <div className="h-12 border-b border-b-white text-white">
-      SearchBar
+      <form onSubmit={((e)=> {e.preventDefault; setGuessState(e.target.value)})}>
+        <input type="search" placeholder="Search..." />
+        <button>Search</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default GameInput
+export default GameInput;
--- src/components/game/GameOutput.tsx
+++ src/components/game/GameOutput.tsx
@@ -30,3 +30,4 @@ const GameOutput = ({guessState}: {guessState: string}) => {
 };
 
 export default GameOutput;
+   
