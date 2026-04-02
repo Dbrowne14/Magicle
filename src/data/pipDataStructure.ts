a6c92d036e70e915312a447576e28385f50b03df
Dbrowne14 <davidbrowne1992@gmail.com>
2026-04-02 16:07:19 +0100

    includign mana symbols as images

---
+++ src/data/pipDataStructure.ts
@@ -0,0 +1,15 @@
+import colorless from "../assets/manaSymbols/colorless.svg";
+import plains from "../assets/manaSymbols/plains.svg";
+import island from "../assets/manaSymbols/island.svg";
+import swamp from "../assets/manaSymbols/swamp.svg";
+import mountain from "../assets/manaSymbols/mountain.svg";
+import forest from "../assets/manaSymbols/forest.svg";
+
+export const wubrg = {
+  colorless: colorless,
+  plains: plains,
+  island: island,
+  swamp: swamp,
+  mountain: mountain,
+  forest: forest,
+};
