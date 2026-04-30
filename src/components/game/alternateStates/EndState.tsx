f446b2be419c2b1b0894c3230e176242db5ac74e
Dbrowne14 <davidbrowne1992@gmail.com>
2026-05-01 00:35:49 +0100

    minor changes to endState class

--- src/components/game/alternateStates/EndState.tsx
+++ src/components/game/alternateStates/EndState.tsx
@@ -10,6 +10,7 @@ const EndState = () => {
 
       <img src={todaysWord?.img} className="w-40" />
       <h2 className="w-fit text-center">Today's card is: <br></br><span className="font-bold text-headerOrange">{todaysWord?.name}</span></h2>
+      <h2 className="bg-white py-1 px-2 rounded-2xl text-black">Edhrec Rank: <span className="font-semibold">#{todaysWord?.edhrec_rank}</span></h2>
     </div>
   );
 };
